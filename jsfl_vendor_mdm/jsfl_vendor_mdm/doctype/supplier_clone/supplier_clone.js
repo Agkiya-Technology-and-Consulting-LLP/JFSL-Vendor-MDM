ddc811686eb1a736abbe2b6855888567332f7e28
Abhishek-agkiya <abhishek@agkiya.com>
2024-04-05 14:04:00 +0530

    Code Added

---
+++ jsfl_vendor_mdm/jsfl_vendor_mdm/doctype/supplier_clone/supplier_clone.js
@@ -0,0 +1,21 @@
+// Copyright (c) 2024, krutika@agkiya.com and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Supplier Clone', {
+	// send_back_to_supplier: function(frm) {
+    //     // Pass the current state to the server-side function
+    //     frappe.call({
+    //         method: 'jsfl_vendor_mdm.jsfl_vendor_mdm.supplier_clone.on_send_back_to_supplier',
+    //         args: {
+    //             docname: frm.doc.name,
+    //             state: frm.doc.workflow_state
+    //         },
+    //         callback: function(r) {
+    //             if (r.message) {
+    //                 frappe.show_alert(r.message);
+    //                 frm.reload_doc();
+    //             }
+    //         }
+    //     });
+    // }
+});
