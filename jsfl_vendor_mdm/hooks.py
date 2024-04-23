af49b1e3c30ec0b3af1848760443a3044d2fa935
Abhishek-agkiya <abhishek@agkiya.com>
2024-04-23 11:06:53 +0530

    Fixtures Added

--- jsfl_vendor_mdm/hooks.py
+++ jsfl_vendor_mdm/hooks.py
@@ -227,3 +227,9 @@ app_license = "mit"
 # 	"Logging DocType Name": 30  # days to retain logs
 # }
 
+fixtures = [
+    "Workflow",
+    "Workflow State",
+    "Workflow Action",
+    "Workflow Action Master"
+]
