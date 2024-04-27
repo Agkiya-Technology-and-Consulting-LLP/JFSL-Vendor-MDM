9e6933def603caee276bac3894282724da3f7779
Wdgaonkar-Krutika <krutika@agkiya.com>
2024-04-27 12:01:00 +0530

    Has/get permission to company user,L1 manager,MDM manager and frees window for multiple user

---
+++ jsfl_vendor_mdm/jsfl_vendor_mdm/custom/permissions.py
@@ -0,0 +1,26 @@
+import frappe
+
+def has_permission(doc, user):
+    if user == "Administrator" or "System Manager" in frappe.get_roles():
+        return True
+    elif doc.doctype == "Supplier Clone":
+        if user == "Administrator" or (("Company User Team" in frappe.get_roles()) and (doc.workflow_state =='Approval Pending By Company User Team' or doc.workflow_state=='Pushed Back By MDM Manager' or doc.workflow_state=='Pushed Back By L1 Manager' or doc.workflow_state =='Approval Pending By L1 Manager' or doc.workflow_state =='Approval Pending By MDM Manager')):
+            return True
+        elif user == "Administrator" or (("L1 Manager" in frappe.get_roles()) and (doc.workflow_state =='Approval Pending By L1 Manager' or doc.workflow_state=='Pushed Back By L1 Manager' or doc.workflow_state =='Approval Pending By MDM Manager')):
+            return True
+        elif user == "Administrator" or (("MDM Manager" in frappe.get_roles()) and (doc.workflow_state =='Approval Pending By MDM Manager' or doc.workflow_state=='Pushed Back By MDM Manager')):
+            return True
+        else:
+            return False
+   
+    
+def get_permission_query_conditions(user):
+    if user == "Administrator" or "System Manager" in frappe.get_roles():
+        return ""
+    else:
+        if ("Company User Team" in frappe.get_roles()):
+            return """(`tabSupplier Clone`.workflow_state = 'Approval Pending By Company User Team' or `tabSupplier Clone`.workflow_state = 'Pushed Back By MDM Manager' or `tabSupplier Clone`.workflow_state = 'Pushed Back By L1 Manager') """
+        elif ("L1 Manager" in frappe.get_roles()):
+            return """(`tabSupplier Clone`.workflow_state = 'Approval Pending By L1 Manager')"""
+        elif ("MDM Manager" in frappe.get_roles()):
+            return """(`tabSupplier Clone`.workflow_state = 'Approval Pending By MDM Manager')"""
